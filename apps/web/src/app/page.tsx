d0bdbce4b78c6c9fbb97b9a267a39763f7ba763f
Ayaan <ayaanmemon1912@gmail.com>
2025-09-10 12:10:08 +0530

    basic_react_flow

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,31 +1,54 @@
 "use client"
 
-const TITLE_TEXT = `
- ██████╗ ███████╗████████╗████████╗███████╗██████╗
- ██╔══██╗██╔════╝╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗
- ██████╔╝█████╗     ██║      ██║   █████╗  ██████╔╝
- ██╔══██╗██╔══╝     ██║      ██║   ██╔══╝  ██╔══██╗
- ██████╔╝███████╗   ██║      ██║   ███████╗██║  ██║
- ╚═════╝ ╚══════╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝
+import { ReactFlow, Background, Controls,   type Node,
+  type Edge, addEdge, type Connection, useNodesState, useEdgesState, type OnConnect } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { useCallback, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
- ████████╗    ███████╗████████╗ █████╗  ██████╗██╗  ██╗
- ╚══██╔══╝    ██╔════╝╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝
-    ██║       ███████╗   ██║   ███████║██║     █████╔╝
-    ██║       ╚════██║   ██║   ██╔══██║██║     ██╔═██╗
-    ██║       ███████║   ██║   ██║  ██║╚██████╗██║  ██╗
-    ╚═╝       ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
- `;
+const initialNodes: Node[] = [
+  
+];
+const initialEdges:Edge[]=[]
 
-export default function Home() {
+
+
+
+export default function App() {
+const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+const handleAddNode=()=>{
+  setNodes([...nodes,{
+    id: uuidv4() ,
+    position: { x: 0, y: 0 },
+    data: { label: `Node ${uuidv4()}` },
+
+  }])
+
+}
+
+ const onConnect: OnConnect = useCallback(
+    (connection) =>{
+      console.log(connection);
+      setEdges((eds) => addEdge(connection, eds))},
+    [setEdges],
+  );
 
   return (
-    <div className="container mx-auto max-w-3xl px-4 py-2">
-      <pre className="overflow-x-auto font-mono text-sm">{TITLE_TEXT}</pre>
-      <div className="grid gap-6">
-        <section className="rounded-lg border p-4">
-          <h2 className="mb-2 font-medium">API Status</h2>
-        </section>
-      </div>
+    <div style={{ height: '100%', width: '100%' }}>
+      <button className='border text-blue-300 bg-black' onClick={handleAddNode}>add Node</button>
+
+      <ReactFlow 
+      edges={edges} 
+      nodes={nodes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}>
+        <Background />
+        <Controls  />
+      </ReactFlow>
+
     </div>
   );
 }
